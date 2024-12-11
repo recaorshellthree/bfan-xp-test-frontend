c28bf05509ce63a8323576e4a281c82ec5c4a854
redcarrot0803 <redcarrot0803@gmail.com>
2024-12-11 08:05:14 +0800

    feat: Add `.env.example`

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/tasks';
+const API_URL = process.env.REACT_APP_API_URL ?? '';
 
 export const fetchTasks = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(`${API_URL}tasks`);
   return response.data;
 };
 
@@ -12,11 +12,11 @@ export const createTask = async (task: {
   description: string;
   done: boolean;
 }) => {
-  const response = await axios.post(API_URL, task);
+  const response = await axios.post(`${API_URL}tasks`, task);
   return response.data;
 };
 
 export const deleteTask = async (id: string) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
+  const response = await axios.delete(`${API_URL}tasks/${id}`);
   return response.data;
 };
